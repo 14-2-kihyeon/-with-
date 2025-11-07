bdedbd273a4ad93949ef8777a91444516151477a
규규성성 <rbtjd200@gmail.com>
2025-11-07 15:25:25 +0900

    create accounts

--- Finflow/urls.py
+++ Finflow/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls'))
-    path('finances/', include('finances.urls'))
-    path('posts/', include('posts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('finances/', include('finances.urls')),
+    path('posts/', include('posts.urls')),
 
 ]
